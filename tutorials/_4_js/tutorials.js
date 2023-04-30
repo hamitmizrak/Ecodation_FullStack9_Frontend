d6aaad3760c92ac0428337a4af148998be715904
hamitmizrak <hamitmizrak@gmail.com>
2023-04-30 11:46:55 +0300

    JS

--- tutorials/_4_js/tutorials.js
+++ tutorials/_4_js/tutorials.js
@@ -1,1 +1,63 @@
-alert("deneme")
+// alert("deneme")
+
+// Yorumlama
+// <!-- --> HTML
+// /* */  CSS
+// //     JS
+/*multiple Comment*/  
+
+// Çıktı 
+// console.log("console");
+// console.error("console error");
+// console.warn("console warn");
+// console.info("console info");
+
+// alert("Pop-pup-1");
+// window.alert("Pop-pup-2")
+
+// document.writeln("<br/>Ekran44");
+// window.document.writeln("<br/>Ekran23");
+
+// "" => çift Tırnak
+// '' => tek  Tırnak
+// `` => Back Tick
+
+// JS Variable
+// var let const 
+// var ==Global ve aynı isimde yazabilirsin.
+var numberData=11;
+var numberData=22;
+console.log(numberData)
+
+// ES6(2015)
+// let ==Local ve aynı isimde yazamazsın.
+let numberData2=33;
+let numberData3=44;
+numberData3=55;
+console.log(numberData3)
+
+// const ==Local ve aynı isimde yazamazsın ve sabit için kullanırız.
+const numberData4=33;
+const numberData5=44;
+//numberData5=44; yazamazsınız.
+console.log(numberData5)
+
+// değişken adlanadırma (naming convensiton)
+// camelCase kuralına
+let _$degiskenAdi44="Merhabalar";
+console.log(_$degiskenAdi44)
+
+//yazamasınız 55degiskenAdi
+//yazamasınız %degiskenAdi
+//yazamasınız degiskenAdi
+let degiskenAdi96üğşçö="Merhabalar96";
+console.log(degiskenAdi96üğşçö);
+
+// typeof
+let data=true;
+let dataTypeOf=typeof data
+console.log(dataTypeOf);
+data="Nasılsınız";
+console.log(typeof data);
+data=55;
+console.log(typeof data);
