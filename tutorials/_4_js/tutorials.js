79b67caa2a0fc298688334805684a2a6535cc9b4
hamitmizrak <hamitmizrak@gmail.com>
2023-04-30 13:10:43 +0300

    math, random, infinity,NaN, undefined

--- tutorials/_4_js/tutorials.js
+++ tutorials/_4_js/tutorials.js
@@ -22,108 +22,152 @@
 // '' => tek  Tırnak
 // `` => Back Tick
 
-// JS Variable
-// var let const 
-// var ==Global ve aynı isimde yazabilirsin.
-var numberData=11;
-var numberData=22;
-console.log(numberData)
+// // JS Variable
+// // var let const 
+// // var ==Global ve aynı isimde yazabilirsin.
+// var numberData=11;
+// var numberData=22;
+// console.log(numberData)
 
-// ES6(2015)
-// let ==Local ve aynı isimde yazamazsın.
-let numberData2=33;
-let numberData3=44;
-numberData3=55;
-console.log(numberData3)
+// // ES6(2015)
+// // let ==Local ve aynı isimde yazamazsın.
+// let numberData2=33;
+// let numberData3=44;
+// numberData3=55;
+// console.log(numberData3)
 
-// const ==Local ve aynı isimde yazamazsın ve sabit için kullanırız.
-const numberData4=33;
-const numberData5=44;
-//numberData5=44; yazamazsınız.
-console.log(numberData5)
+// // const ==Local ve aynı isimde yazamazsın ve sabit için kullanırız.
+// const numberData4=33;
+// const numberData5=44;
+// //numberData5=44; yazamazsınız.
+// console.log(numberData5)
 
-// değişken adlanadırma (naming convensiton)
-// camelCase kuralına
-let _$degiskenAdi44="Merhabalar";
-console.log(_$degiskenAdi44)
+// // değişken adlanadırma (naming convensiton)
+// // camelCase kuralına
+// let _$degiskenAdi44="Merhabalar";
+// console.log(_$degiskenAdi44)
 
-//yazamasınız 55degiskenAdi
-//yazamasınız %degiskenAdi
-//yazamasınız degiskenAdi
-let degiskenAdi96üğşçö="Merhabalar96";
-console.log(degiskenAdi96üğşçö);
+// //yazamasınız 55degiskenAdi
+// //yazamasınız %degiskenAdi
+// //yazamasınız degiskenAdi
+// let degiskenAdi96üğşçö="Merhabalar96";
+// console.log(degiskenAdi96üğşçö);
 
-// typeof
-let data=true;
-let dataTypeOf=typeof data
-console.log(dataTypeOf);
-data="Nasılsınız";
-console.log(typeof data);
-data=55;
-console.log(typeof data);
-
-
-// whoisting
-firstWrite="Merhabalar99";
-console.log(typeof firstWrite);
-var firstWrite;
-
-// variable initialization
-let data44="Kelime";
-console.log(data44);
-
-// operator
-let number1=11,number2=2;
-console.log("Toplam =>"+ (number1+number2))
-console.log("Çıkarma =>"+ (number1-number2))
-console.log("Çarpma =>"+ (number1*number2))
-console.log("Bölme =>"+ (number1/number2))
-console.log("Kalan =>"+ (number1%number2))
-
-// &&=VE ||=VEYA  ! DEĞİL  !=EŞİT DEĞİL
-
-//ARTIRMA (+) AZALMA(-)
-let counter=1;
-//counter++;
-//counter=counter+1;
-counter+=1;
-console.log(counter)
-// ++ = 1 artır
-// ++ = 1 artır
-// ++ = 1 artır
-// X++ ==> İşlemden sonra topla        (postfix increment)
-// ++X ==> önce topla sonra işlem yap  (prefix increment)
+// // typeof
+// let data=true;
+// let dataTypeOf=typeof data
+// console.log(dataTypeOf);
+// data="Nasılsınız";
+// console.log(typeof data);
+// data=55;
+// console.log(typeof data);
 
 
-// typeof
-let data96=true;
-// dizinin Türü nedir ?
-console.log(typeof data96)
+// // whoisting
+// firstWrite="Merhabalar99";
+// console.log(typeof firstWrite);
+// var firstWrite;
 
-// instanceof
-let dizi=["Malatya","Elazığ","Urfa"];
-// dizinin Türü Array mi?
-console.log(dizi instanceof Array); // output => true
-console.log(dizi instanceof Object);// output => true
-console.log(dizi instanceof Number);// output => false
-console.log(dizi instanceof String);// output => false
+// // variable initialization
+// let data44="Kelime";
+// console.log(data44);
 
-// prompt
-let userData=prompt("Lütfen birşeyler yazınız");
-alert(userData);
+// // operator
+// let number1=11,number2=2;
+// console.log("Toplam =>"+ (number1+number2))
+// console.log("Çıkarma =>"+ (number1-number2))
+// console.log("Çarpma =>"+ (number1*number2))
+// console.log("Bölme =>"+ (number1/number2))
+// console.log("Kalan =>"+ (number1%number2))
 
-// cast
-let userData2=String(prompt("Lütfen birşeyler yazınız")) ;
-alert(userData2);
+// // &&=VE ||=VEYA  ! DEĞİL  !=EŞİT DEĞİL
 
-let userData3=Number(prompt("Lütfen bir sayı yazınız")) ;
-alert(Math.sqrt(userData3) );
+// //ARTIRMA (+) AZALMA(-)
+// let counter=1;
+// //counter++;
+// //counter=counter+1;
+// counter+=1;
+// console.log(counter)
+// // ++ = 1 artır
+// // ++ = 1 artır
+// // ++ = 1 artır
+// // X++ ==> İşlemden sonra topla        (postfix increment)
+// // ++X ==> önce topla sonra işlem yap  (prefix increment)
 
 
+// // typeof
+// let data96=true;
+// // dizinin Türü nedir ?
+// console.log(typeof data96)
+
+// // instanceof
+// let dizi=["Malatya","Elazığ","Urfa"];
+// // dizinin Türü Array mi?
+// console.log(dizi instanceof Array); // output => true
+// console.log(dizi instanceof Object);// output => true
+// console.log(dizi instanceof Number);// output => false
+// console.log(dizi instanceof String);// output => false
+
+// // prompt
+// let userData=prompt("Lütfen birşeyler yazınız");
+// alert(userData);
+
+// // cast
+// let userData2=String(prompt("Lütfen birşeyler yazınız")) ;
+// alert(userData2);
+
+// let userData3=Number(prompt("Lütfen bir sayı yazınız")) ;
+// alert(Math.sqrt(userData3) );
+
+////////////////////////////////////////////////////////////////////
 // undefined
+let username;
+console.log(username);
+
 // NaN
+console.log(4/4);
+console.log(4/"asd");
+
 // infinity
-// new 
-// this
-// null
+console.log(5/0);
+
+// new : doğmak
+// null: ölmek
+// zero: hayatta ama hiç bir şeyi kalmamış
+// this: bu anlamaına objeyi işaretleme için kullanıyoruz.
+
+////////////////////////////////////////////////////////////////////
 // Math
+console.log(Math.PI);
+console.log(Math.E);
+
+console.log(Math.min(2,5,6,88,100,-500));
+console.log(Math.max(2,5,6,88,100,-500));
+
+console.log(Math.abs(-44));
+console.log(Math.sqrt( Math.abs(-25)));
+
+console.log(Math.pow(2,5));
+console.log(Math.pow(2,Math.sqrt( Math.abs(-25))));
+
+console.log(Math.ceil(7.1));
+console.log(Math.floor(6.9));
+console.log(Math.round(3.4));
+console.log(Math.round(3.5));
+
+//rastgele 
+console.log(Math.floor(Math.random()*9+1)); // 1<=X<=9
+
+//// MATH ÖDEVİ
+// Math Ödev
+// Kullanıcıda tarafından aldığımız 2 tane sayının (prompt)
+// s1-) Bu sayılardan en küçüğü nedir ? (min)
+// s2-) Bu sayılardan en büyüğü nedir ? (max)
+// s3-) Bu sayılardan en büyüğü karekökü nedir ? (sqrt(max))
+// s4-) Bu sayılardan en büyüğü karekökünün üste yuvarlıyalım nedir ? (ceil(sqrt(max)))
+// s5-) Bu sayılardan en küçüğü mutlak değeri nedir ? (abs(min))
+// s6-) Bu sayılardan en küçüğü alt taban en büyüğü üst taban şeklinde üstünü alalım(pow(min,max))
+
+
+
+
